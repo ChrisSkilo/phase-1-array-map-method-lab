381829eb6ea2e91afbdc4fd780f9d05122a0ca1f
Chrispus <skilochris@gmail.com>
2023-10-16 11:00:41 +0300

    assignment completed

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+   const titleCaseWords = words.map(
+     word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+     });
+    return titleCaseWords.join(' ');
+   })
+   
+  }
+const result = titleCased()
+console.log(result)
